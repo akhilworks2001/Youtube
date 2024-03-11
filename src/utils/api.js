212ec285c5a1a3864e777567f2f70b59c48f50ee
PIYUSH\piyush <piyush98verma@gmail.com>
2024-03-11 17:05:30 +0530

    first commit

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key": '1f592118b2msh974f8b8ac2aa8d7p1020e3jsn61552cfb789d',
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
